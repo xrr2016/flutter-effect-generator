632b0d600b4640b12494961f8aa733064b1c493f
冷石 Boy <xiaoranran1993@outlook.com>
2021-12-09 16:52:38 +0800

    feat(charts): update view

--- lib/src/charts/charts_view.dart
+++ lib/src/charts/charts_view.dart
@@ -40,7 +40,7 @@ class _ChartsViewState extends State<ChartsView> {
           setState(() {});
         },
         style: TextStyle(
-          color: Colors.white,
+          color: Colors.black,
         ),
         keyboardType: TextInputType.number,
       ),
@@ -57,7 +57,7 @@ class _ChartsViewState extends State<ChartsView> {
         },
         title: Text(
           ChartType.values[index].name,
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.black),
         ),
         contentPadding: EdgeInsets.all(10.0),
         selected: ChartType.values[index] == _chartsController.chartType,
@@ -174,6 +174,10 @@ class _ChartsViewState extends State<ChartsView> {
     return Scaffold(
       appBar: AppBar(
         actions: [],
+        foregroundColor: Colors.black,
+        title: Text('Charts'),
+        backgroundColor: Color(0xffefeeee),
+        elevation: 1.0,
       ),
       body: AnimatedBuilder(
         animation: _chartsController,
@@ -185,29 +189,69 @@ class _ChartsViewState extends State<ChartsView> {
               children: [
                 Container(
                   width: 200.0,
-                  color: Colors.blueAccent,
+                  color: Color(0xffefeeee),
                   child: ListView(
                     padding: EdgeInsets.all(10.0),
                     children: _renderChartNames(),
                   ),
                 ),
+                VerticalDivider(
+                  width: 1.0,
+                  color: Colors.grey,
+                ),
                 Expanded(
-                  child: Stack(
-                    children: [
-                      // GridBackground(),
-                      Center(
-                        child: SizedBox(
-                          child: _renderChart(),
-                          width: 720.0,
-                          height: 640.0,
+                  child: Container(
+                    padding: EdgeInsets.all(100.0),
+                    color: Color(0xffefeeee),
+                    alignment: Alignment.center,
+                    transformAlignment: Alignment.center,
+                    child: Container(
+                      color: Color(0xffefeeee),
+                      child: Container(
+                        child: Center(
+                          child: SizedBox(
+                            width: 720,
+                            height: 640,
+                            child: _renderChart(),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color(0xffefeeee),
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Color(0xffefeeee),
+                              Color(0xffefeeee),
+                            ],
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xffffffff),
+                              offset: Offset(-20.0, -20.0),
+                              blurRadius: 30,
+                              spreadRadius: 0.0,
+                            ),
+                            BoxShadow(
+                              color: Color(0xffd1d0d0),
+                              offset: Offset(20.0, 20.0),
+                              blurRadius: 30,
+                              spreadRadius: 0.0,
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
+                ),
+                VerticalDivider(
+                  width: 1.0,
+                  color: Colors.grey,
                 ),
                 Container(
                   width: 200.0,
-                  color: Colors.blueAccent,
+                  color: Color(0xffefeeee),
                   child: Column(
                     children: [
                       Expanded(
@@ -228,3 +272,51 @@ class _ChartsViewState extends State<ChartsView> {
     );
   }
 }
+
+
+      // Container(
+      //   width: 500.0,
+      //   height: 500.0,
+      //   color: Color(0xffefeeee),
+      //   alignment: Alignment.center,
+      //   transformAlignment: Alignment.center,
+      //   child: Container(
+      //     color: Color(0xffefeeee),
+      //     child: Container(
+      //       width: 200,
+      //       height: 200,
+      //       child: Icon(
+      //         Icons.star,
+      //         size: 67,
+      //         color: Colors.amber,
+      //       ),
+      //       decoration: BoxDecoration(
+      //         color: Color(0xffefeeee),
+      //         borderRadius: BorderRadius.circular(10),
+      //         gradient: LinearGradient(
+      //           begin: Alignment.topLeft,
+      //           end: Alignment.bottomRight,
+      //           colors: [
+      //             Color(0xffefeeee),
+      //             Color(0xffefeeee),
+      //           ],
+      //         ),
+      //         boxShadow: [
+      //           BoxShadow(
+      //             color: Color(0xffffffff),
+      //             offset: Offset(-20.0, -20.0),
+      //             blurRadius: 30,
+      //             spreadRadius: 0.0,
+      //           ),
+      //           BoxShadow(
+      //             color: Color(0xffd1d0d0),
+      //             offset: Offset(20.0, 20.0),
+      //             blurRadius: 30,
+      //             spreadRadius: 0.0,
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // )
+  
